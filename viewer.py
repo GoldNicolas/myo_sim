e265a5b3c9e6e1563f1b41183151b4ae900950e8
GoldNicolas <nicolas.guerra0804@gmail.com>
2025-03-27 11:40:36 -0500

    save

--- viewer.py
+++ viewer.py
@@ -12,7 +12,7 @@ from mujoco.viewer import launch
 # 
 # model = mujoco.MjModel.from_xml_path("myo_sim/arm/leftarmtest.xml") 
 
-model = mujoco.MjModel.from_xml_path("myo_sim/arm/leftarm.xml") 
+model = mujoco.MjModel.from_xml_path("C:\Users\Usuario\Desktop\idk\myo_sim\hand\myohand.xml") 
 
 data = mujoco.MjData(model)
 
